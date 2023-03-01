794c9a2f094fd6e61491353db75baecfd248e3fe
roee korot <korotroee@gmail.com>
2023-03-01 15:46:22 +0200

    created header component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WeatherService } from './services/weather.service';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    HeaderComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent
   ],
   providers: [
     WeatherService
